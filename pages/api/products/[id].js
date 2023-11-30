86e8066a2db8945e962e099276888202d58777fc
Franziska Adam <franziskacarolineadam@gmail.com>
2023-11-30 11:20:09 +0100

    Solve Detail Page

--- pages/api/products/[id].js
+++ pages/api/products/[id].js
@@ -2,5 +2,6 @@ import { getProductById } from "@/services/productServices";
 
 export default function handler(request, response) {
   const { id } = request.query;
-  response.status(200).json(getProductById(id));
+
+  return response.status(200).json(getProductById(id));
 }
